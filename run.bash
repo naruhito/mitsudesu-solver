aa15dbe731ebb7c2882dfbbd5c6ad92a09f8737b
Naruhito <naruhito78@gmail.com>
2020-04-21 01:13:30 +0900

    update run.bash

--- run.bash
+++ run.bash
@@ -4,27 +4,37 @@ set -e
 NAME="mitsudesu-solver"
 IMAGE="${NAME}:latest"
 
-for PROG in "docker" "vncviewer"; do
-  if ! which ${PROG} > /dev/null 2>&1; then
-    echo "${PROG} is not installed."
+function main() {
+  check-requirements
+  create-container
+  run-vncviewer
+}
+
+function check-requirements() {
+  for PROG in "docker" "vncviewer"; do
+    if ! which ${PROG} > /dev/null 2>&1; then
+      echo "${PROG} is not installed."
+      exit 1
+    fi
+  done
+  if [ -z "${DISPLAY}" ]; then
+    echo "DISPLAY is not available."
     exit 1
   fi
-done
+}
 
-if [ -z "${DISPLAY}" ]; then
-  echo "DISPLAY is not available."
-  exit 1
-fi
+function create-container() {
+  CONTAINER=$(docker run -d --rm --name ${NAME} -v ${PWD}/docker:/repo ${IMAGE})
+  trap "docker rm -f ${CONTAINER}" 0 2 3 15
+  while ! docker exec ${CONTAINER} ps | grep x11vnc > /dev/null 2>&1; do
+    docker logs ${CONTAINER}
+    sleep 1
+  done
+}
 
-CONTAINER=$(docker run -d --rm --name ${NAME} -v ${PWD}/docker:/repo ${IMAGE})
+function run-vncviewer() {
+  IP=$(docker inspect ${CONTAINER} | grep -E --color=never "IPAddress.+172" | cut -d\" -f4 | sort | uniq)
+  vncviewer ${IP}
+}
 
-trap "docker rm -f ${CONTAINER}" 0 2 3 15
-
-while ! docker exec ${CONTAINER} ps | grep x11vnc > /dev/null 2>&1; do
-  docker logs ${CONTAINER}
-  sleep 1
-done
-
-IP=$(docker inspect ${CONTAINER} | grep -E --color=never "IPAddress.+172" | cut -d\" -f4 | sort | uniq)
-
-vncviewer ${IP}
+main
